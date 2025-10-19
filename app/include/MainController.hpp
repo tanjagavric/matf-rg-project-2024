713720f14cdaa36bf8b60027b8a58b8215ef31aa
Tanja Gavric <tanja2000gavric@gmail.com>
2025-10-19 17:08:28 +0000

    Close app on ESC

--- app/include/MainController.hpp
+++ app/include/MainController.hpp
@@ -10,6 +10,8 @@ namespace app {
     class MainController : public engine::core::Controller {
         void initialize() override;
 
+        bool loop() override;
+
     public:
         std::string_view name() const override {
             return "app::MainController";
--- app/src/MainController.cpp
+++ app/src/MainController.cpp
@@ -4,7 +4,17 @@
 
 #include "MainController.hpp"
 
+#include <engine/platform/PlatformController.hpp>
+
 namespace app {
     void MainController::initialize() {
+    }
+
+    bool MainController::loop() {
+        auto platform = get<engine::platform::PlatformController>();
+        if (platform->key(engine::platform::KEY_ESCAPE).is_down()) {
+            return false;
+        }
+        return true;
     }
 } // app
